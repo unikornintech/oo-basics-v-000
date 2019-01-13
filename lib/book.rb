122592a7c4b4c116fdd480cdfceb6e49eb11fe5d
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 17:07:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -18,6 +18,14 @@
      @author
    end 
    
+   def page_count(num)
+     @page_count = num 
+   end 
+   
+   def page_count
+     @page_count
+   end 
+   
  end 
 
 Book.new("And Then There Were None")
