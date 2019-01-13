872a06efa556d1732a40b7977070776d1e74d07e
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 17:21:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,42 +1,18 @@
  # book.rb 
  
- class Book 
+ class Book
+   attr_accessor :author, :page_count, :genre 
    
    def initialize(title)
      @title = title
-   end
+   end 
    
    def title
      @title
    end 
    
-   def author=(author)
-     @author = author
-   end
-   
-   def author
-     @author
-   end 
-   
-   def page_count=(num)
-     @page_count = num 
-   end 
-   
-   def page_count
-     @page_count
-   end 
-   
-   def genre=(genre)
-     @genre = genre
-   end 
-   
-   def genre
-     @genre
-   end 
-   
    def turn_page
      puts "Flipping the page...wow, you read fast!"
    end 
+   
  end 
-
-Book.new("And Then There Were None")
