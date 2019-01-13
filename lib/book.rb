0c3b8b96aeddaf0d8fc5de8d8e3a7e55f116765b
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 17:02:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+ # book.rb 
+ 
+ class Book 
+   
+   def initialize(title)
+     @title = title
+   end
+   
+   def title
+     @title
+   end 
+   
+   def author=(author)
+     @author = author
+   end 
+   
+ end 
 
-
+Book.new("And Then There Were None")
