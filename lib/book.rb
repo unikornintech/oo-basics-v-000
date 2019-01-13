e285867645dea845499f0082da6a77012bc8ca05
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 17:22:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,13 +2,10 @@
  
  class Book
    attr_accessor :author, :page_count, :genre 
+   attr_reader :title 
    
    def initialize(title)
      @title = title
-   end 
-   
-   def title
-     @title
    end 
    
    def turn_page
